59791fb0784838fcd5b086f5e58c40ce63495b50
mzwartz <mtzwartz@gmail.com>
2020-11-12 16:20:16 -0600

    Create sum_two_numbers.c

---
+++ Challenges/Zwartz/sum_two_numbers.c
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <csstudio>
+
+using namespace std;
+
+int main()
+{
+int x;
+int y;
+int sum;
+
+cout << "Input your two numbers";
+
+cin << x;
+cin << y;
+
+sum = x + y
+
+cout << x << "+" << y << "=";
+
+
+}
