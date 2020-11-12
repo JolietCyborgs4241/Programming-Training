abb9376508cda17293bb298741ad636759b1fd0f
Alex Herzog <alexgherzog@gmail.com>
2020-11-12 16:42:37 -0600

    Sum of Two Numbers Challenge

---
+++ Challenges/Very Easy Challenges/Alex/Sum of Two Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int num1;
+    int num2;
+    cout << "Enter Two Numbers to be added" << endl;
+    cin >> num1;
+    cin >> num2;
+
+    int solution;
+    solution = num1 + num2;
+    cout << "The answer is: " << solution << endl;
+
+    return 0;
+}
