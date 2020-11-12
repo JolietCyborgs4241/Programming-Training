d3f3fff973bd2eaf403762b10b61c6e7b19b8d86
Megan <53574078+Qtrmegan@users.noreply.github.com>
2020-11-12 16:49:31 -0600

    Update Untitled1.c

--- Challenges/Very Easy Challenges/Megan/Untitled1.c
+++ Challenges/Very Easy Challenges/Megan/Untitled1.c
@@ -1,2 +1,27 @@
 #include <iostream>
+#include <cstudio>
+
+using namespace std;
+
+ int addition() {
+ 
+ int a;
+ int b;
+ 
+ 
+cout<<" input number" << endl;
+cin>>a;
+cin>>b;
+
+ int sum = a + b;
+ cout<< "sum = "<<sum<< endl;
+ 
+ 
+ 
+ 
+ return 0; 
+ 
+
+ }
+
 
