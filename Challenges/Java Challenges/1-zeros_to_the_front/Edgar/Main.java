4503aa30a70c3b94ea867e06ea359e7891887a70
MattPapesh <55628349+MattPapesh@users.noreply.github.com>
2021-09-22 11:55:54 -0500

    Update Main.java

--- Challenges/Java Challenges/1-zeros_to_the_front/Edgar/Main.java
+++ Challenges/Java Challenges/1-zeros_to_the_front/Edgar/Main.java
@@ -2,26 +2,41 @@ public class Main
 {
     static int[][] arrays =
     {
-        new int[]{1, 2, 9, 5, 6},
-        new int[]{},
-        new int[]{},
-        new int[]{},
-        new int[]{},
-        new int[]{},
-        new int[]{},
-        new int[]{},
-        new int[]{},
+        new int[]{2, 4, 12, 0, 3, 0, 0, 2, 1},
+        new int[]{0, 2, 0, -1, -9, 6, 0, 0, 1},
+        new int[]{1, 2, 9, -1, -9, 8, 12, 8, -8},
+        new int[]{0, 0, 0, 1, 2, 3, 0, 0, 0},
+        new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0},
+        new int[]{0, 0, 0, 0, 0, 0, 0, 0, 1},
+        new int[]{1, 0, 9, -3, -3, 1, 0, 9, -10},
+        new int[]{1, 0,-6, 6, 0, 0, 8, 0, 3},
+        new int[]{-1, 9, 0, 7, -13, 0, 8, -2, 0},
     };
 
     static zeros_to_the_front program = new zeros_to_the_front(); 
 
+    static int getLargestArrayLength(int[][] arrays)
+    {
+    	int largest_length = 0;
+    	
+    	for(int i = 0; i < arrays.length; i++)
+    	{
+    		if(arrays[i].length > largest_length)
+    		{
+    			largest_length = arrays[i].length; 
+    		}
+    	}
+    	
+    	return largest_length; 
+    }
+    
     static void displayArray(int[] array)
     {
         System.out.print("{ ");
 
         for(int i = 0; i < array.length; i++)
         {
-            if(i != 0 && i != array.length - 1)
+            if(i != 0 && i != array.length)
             {
                 System.out.print(", ");
             }
@@ -31,47 +46,17 @@ public class Main
 
         System.out.print(" }"); 
     }
-
-    static int getNumOfZeros(int[] array)
-    {
-        int num_of_zeros = 0; 
-
-        for(int i = 0; i < array.length; i++)
-        {
-            if(array[i] == 0)
-            {
-                num_of_zeros++;
-            }
-        }
-
-        return num_of_zeros; 
-    }
-
+    
     static void runTest(int[] array, int trial)
     {
-        int[] updated_array = new zeros_to_the_front().getUpdatedArray(array); 
+    	String line_adjustment = ""; //Adjust a printed line based on the lengths of the arrays
+    	int[] updated_array = new zeros_to_the_front().getUpdatedArray(array); 
 
         System.out.print("Test " + trial + " -> BEFORE: ");
         displayArray(array); 
-        System.out.print("    AFTER: "); 
+        System.out.print(line_adjustment + "	    AFTER: "); 
         displayArray(updated_array);
-        System.out.print("    CONCLUSION: "); 
-
-        for(int i = 0; i < getNumOfZeros(updated_array); i++)
-        {
-            try
-            {
-                if(updated_array[i] != 0)
-                {
-                    System.out.println("FAILED \n");
-                    return; 
-                }
-            }
-
-            catch(IndexOutOfBoundsException e){}
-        }
-        
-        System.out.println("SUCCESSFUL \n");
+        System.out.println("\n");
     }
 
     public static void main(String[] args)
