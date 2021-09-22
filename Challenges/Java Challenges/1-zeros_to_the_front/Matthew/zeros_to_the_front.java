934cebff93cf62437c4aaa6a67f1663272a39346
MattPapesh <55628349+MattPapesh@users.noreply.github.com>
2021-09-22 12:03:58 -0500

    Update zeros_to_the_front.java

--- Challenges/Java Challenges/1-zeros_to_the_front/Matthew/zeros_to_the_front.java
+++ Challenges/Java Challenges/1-zeros_to_the_front/Matthew/zeros_to_the_front.java
@@ -14,9 +14,17 @@ public class zeros_to_the_front
 
     private void execute()
     {
-        // all you need is the array and length of the array to write the programe;
-        // these variable are already declared above. 
-
-        // write code here:
+        int removed_value = 0, removed_value_index = 0; 
+  
+        for(int current_index = 0; current_index < length; current_index++)
+        {
+        	if(array[current_index] == 0)
+        	{
+        		removed_value = array[removed_value_index];
+        		array[removed_value_index] = 0;
+        		array[current_index] = removed_value;
+        		removed_value_index++; 
+        	}
+        }
     }
 }
