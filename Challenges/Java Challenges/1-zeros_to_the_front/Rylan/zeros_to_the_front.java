71981e95a11decdb556bf0a1edaf695c7e6e7c12
Rylan C <89877940+Rylan0777@users.noreply.github.com>
2021-09-30 17:32:39 -0500

    Update zeros_to_the_front.java

--- Challenges/Java Challenges/1-zeros_to_the_front/Rylan/zeros_to_the_front.java
+++ Challenges/Java Challenges/1-zeros_to_the_front/Rylan/zeros_to_the_front.java
@@ -14,9 +14,18 @@ public class zeros_to_the_front
 
     private void execute()
     {
-        // all you need is the array and length of the array to write the programe;
-        // these variable are already declared above. 
-
-        // write code here:
+    	int soup = 0; 
+    	int saved = 0;
+    	for (int i = 0; i < length; i++)
+    	{
+    		if (array[i] == 0)
+    		{
+    			saved = array[soup];
+    			array[soup] = 0;
+    			array[i] = saved; 
+    			soup++;
+    		}
+    	}
+    		
     }
 }
