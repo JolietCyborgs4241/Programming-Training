a17be6425024627b7849c09e3599aea2130b001c
StrongaCyborgs24 <89878096+StrongaCyborgs24@users.noreply.github.com>
2021-09-30 17:32:14 -0500

    Completed challenge

--- Challenges/Java Challenges/1-zeros_to_the_front/Aaron/zeros_to_the_front.java
+++ Challenges/Java Challenges/1-zeros_to_the_front/Aaron/zeros_to_the_front.java
@@ -1,39 +1,35 @@
-import java.util.Arrays;
-
 public class zeros_to_the_front
 {
     public int[] getUpdatedArray(int[] array)
     {
         this.array = array; 
-        length = array.length; 
+        length = array.length;
         execute();
+        
 
         return this.array; 
     }
 
     private int[] array = {};
     private int length = 0; 
+    int saved_num = 0;
+    int left_most_index = 0;
 
     private void execute()
     {		
     	for(int i = 0; i < length; i++) 
     	{
-    		
     		if(array[i] == 0)
     		{
-    			
+    			saved_num = array[left_most_index];
+    			array[left_most_index] = array[i];
+    			array[i] = saved_num;
+    			left_most_index++;
     		}
-    		
-    		
-    		
     		
     	}
     	
     	
     }
 
-	private void sortit(int[] arr, int i) {
-		// TODO Auto-generated method stub
-		
-	}
 }
