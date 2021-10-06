89604fe9665528c95e9f5ce5cca057f696b6ae74
EchoEthan <90357524+EchoEthan@users.noreply.github.com>
2021-10-06 17:24:28 -0500

    Update zeros_to_the_front.java

--- Challenges/Java Challenges/1-zeros_to_the_front/Ethan/zeros_to_the_front.java
+++ Challenges/Java Challenges/1-zeros_to_the_front/Ethan/zeros_to_the_front.java
@@ -1,33 +1,44 @@
-public class zeros_to_the_front
-{
-    public int[] getUpdatedArray(int[] array)
-    {
-        this.array = array; 
-        length = array.length; 
-        execute();
+import lower_level.app;
 
-        return this.array; 
+/* 
+    This is where your app should be created. All code relavent to your app goes in this file, and any
+    images for sprites should be put into the "sprites" folder. 
+
+    When programming, there will always be a main() function; it's what is called(ran) by the operating system 
+    so your computer knows where to start in terms of executing your code. However, this time you will be 
+    treating the "execute()" function as the "main()" or "public static void main(String[] args)" function. 
+    
+    NOTE: You can do really anything in this file, but just don't change anything inside of the
+    appContainer() function!
+
+    "BUILT_IN FUNCTIONS":
+    
+    - createSprite(String sprite_name, String file_name) -> creates a sprite given the file name from the sprites folder.
+    - deleteSprite(String sprite_name) 
+    - setSpritePose(String sprite_name, int x, int y) -> sets a sprite's coordinates, but it may or may 
+                                                           not be visible depending on the sprite's visibility status.
+     
+    - toggleSpriteVisibility(String sprite_name, boolean visible) -> visible = true shows a sprite, and visible = false
+                                                                       hides the sprite.
+     
+    Both functions below are to get the input from your computer's keyboard, but what it returns is an integer and 
+    not a character given that not every key has a character.  
+     
+    - getCurrentKeyPressed()
+    - getLastKeyReleased()
+*/
+    
+
+// All code should be written in here:
+public class appContainer extends app 
+{ 
+    public appContainer()
+    {
+        super();
     }
 
-    private int[] array = {};
-    private int length = 0; 
-
-    private void execute()
+    public void execute()
     {
-        // all you need is the array and length of the array to write the programe;
-        // these variable are already declared above. 
-
-        // write code here: 
-    	int echo = 0;
-    	int ghostly = 0;
-    	for(int i = 0; i > length ; i++)
-    	{
-    		if(array[i] == 0)
-    		{
-    		array[i] =4;
-    		array[4] = 0;
-    		
-    		}
-    	}
+        createSprite("ball," "ball.png")
     }
 }
